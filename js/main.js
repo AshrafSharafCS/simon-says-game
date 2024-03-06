b6931923b552b00b49944481787168fa26812c02
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-06 03:35:47 +0200

    cleaning

--- js/main.js
+++ js/main.js
@@ -3,43 +3,63 @@ const info = document.getElementById("info");
 const colors = ["green", "red", "yellow", "blue"];
 const highScore = document.getElementById("high-score");
 const level = document.getElementById("level");
-let count = 1;
+const board = document.querySelector(".board");
+
+let count = 0;
 let playArray = [];
 let choosenTile;
-const board = document.querySelector(".board");
-const tiles = document.querySelectorAll("[data-tile]");
+
+const red = document.querySelector('[data-tile="red"');
+const blue = document.querySelector('[data-tile="blue"');
+const green = document.querySelector('[data-tile="green"');
+const yellow = document.querySelector('[data-tile="yellow"]');
+
+// red.addEventListener("click", handleclick);
+// blue.addEventListener("click", handleclick);
+// green.addEventListener("click", handleclick);
+// yellow.addEventListener("click", handleclick);
+
 let answerArray = [];
 let game = true;
 
-playButton.addEventListener("click", function () {
-  Startgame();
-});
+
+
+
+
+
+function resetGame() {
+  count=0;
+  playArray=[];
+
+}
 
 function GenerateRandomNumber() {
   let rand = Math.floor(Math.random() * colors.length);
   return rand;
 }
 
-function PlayLevel() {
-  board.classList.remove("unclickable");
-  playArray.forEach((color, index) => {
-    board.addEventListener("click", function (event) {
-      choosenTile = event.target.dataset.tile;
-      answerArray.push(choosenTile);
-      console.log(answerArray);
-     
-    });
-  }); 
+function Playsound(toBePlayed) {
+  const sound = new Audio(`../sounds/${toBePlayed}.mp3`);
+  sound.play();
 }
-function PlayLevel(){
+
+function PlayLevel() {
+
   board.classList.remove("unclickable");
-  
+  playArray.forEach((color,indexinarray)=>{
+    tile = document.querySelector(`[data-tile=${color}]`);
+    tile.addEventListener("click",function(){
+      console.log(color);
+      
+    })
+ })
+
 }
 
 function RunLevel() {
   playArray.forEach((color, indexinarray) => {
     setTimeout(() => {
-      let tile = document.querySelector(`[data-tile=${color}]`);
+      tile = document.querySelector(`[data-tile=${color}]`);
       Playsound(color);
       tile.classList.remove("inactive");
       setTimeout(() => {
@@ -51,14 +71,13 @@ function RunLevel() {
 
 function Startgame() {
   info.innerHTML = "Time to play ! Stay Focused :)";
-  level.textContent = count;
   count += 1;
+  level.textContent = count;
   playArray.push(colors[GenerateRandomNumber()]);
   RunLevel();
-  PlayLevel(playArray);
+  PlayLevel();
 }
 
-function Playsound(toBePlayed) {
-  const sound = new Audio(`../sounds/${toBePlayed}.mp3`);
-  sound.play();
-}
+playButton.addEventListener("click", function () {
+  Startgame();
+});
